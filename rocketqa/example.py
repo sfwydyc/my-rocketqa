c9f6991cff54879f3f0a0fe4a7f4f735843ad7aa
zhuhongyu05 <zhuhongyu05@baidu.com>
2021-10-25 11:19:46 +0800

    add inference model interface

--- rocketqa/example.py
+++ rocketqa/example.py
@@ -3,14 +3,16 @@ import paddle
 import itertools
 import numpy as np
 from predict.dual_encoder import DualEncoder
+from predict.dual_encoder_infer import DualEncoderInfer
 from predict.cross_encoder import CrossEncoder
+from predict.cross_encoder_infer import CrossEncoderInfer
 
 paddle.enable_static()
 
 def EncoderExample(encoder_conf, data_list, side):
     encoder = DualEncoder(**encoder_conf)
     feats = encoder.get_representation(data_list)
-    emb_f = open(side + '_emb', 'wb')
+    emb_f = open(side + '_emb1', 'wb')
     for fff in feats:
         emb_str = ' '.join(str(score) for score in fff) + '\n'
         emb_f.write(emb_str.encode(encoding='utf8'))
@@ -24,6 +26,23 @@ def RankerExample(encoder_conf, data_list):
         score_f.write(str(s) + '\n')
 
 
+def EncoderInferExample(encoder_conf, data_list, side):
+    encoder = DualEncoderInfer(**encoder_conf)
+    feats = encoder.get_representation(data_list)
+    emb_f = open(side + '_emb1', 'wb')
+    for fff in feats:
+        emb_str = ' '.join(str(score) for score in fff) + '\n'
+        emb_f.write(emb_str.encode(encoding='utf8'))
+
+
+def RankerExampleInfer(encoder_conf, data_list):
+    encoder = CrossEncoderInfer(**encoder_conf)
+    scores = encoder.get_rank_score(data_list)
+    score_f = open('score1', 'w')
+    for s in scores:
+        score_f.write(str(s) + '\n')
+
+
 if __name__ == '__main__':
     para_encoder_conf = {
         "conf_path": "/mnt/dqa/dingyuchen/irqa_jina/checkpoints/rocketqa_v1/marco_de_config.json",
@@ -32,7 +51,6 @@ if __name__ == '__main__':
         "batch_size": 32,
         "cls_type": "para"
     }
-
     query_encoder_conf = {
         "conf_path": "/mnt/dqa/dingyuchen/irqa_jina/checkpoints/rocketqa_v1/marco_de_config.json",
         "use_cuda": True,
@@ -40,7 +58,6 @@ if __name__ == '__main__':
         "batch_size": 32,
         "cls_type": "query"
     }
-
     rank_conf = {
         "conf_path": "/mnt/dqa/dingyuchen/irqa_jina/checkpoints/rocketqa_v1/marco_ce_config.json",
         "use_cuda": True,
@@ -48,12 +65,37 @@ if __name__ == '__main__':
         "batch_size": 32
     }
 
+    para_encoder_infer_conf = {
+        "conf_path": "../checkpoints/pair/marco_de_config.json",
+        "use_cuda": True,
+        "gpu_card_id": 1,
+        "batch_size": 32,
+        "cls_type": "para"
+    }
+    query_encoder_infer_conf = {
+        "conf_path": "../checkpoints/pair/marco_de_config.json",
+        "use_cuda": True,
+        "gpu_card_id": 1,
+        "batch_size": 32,
+        "cls_type": "query"
+    }
+    rank_infer_conf = {
+        "conf_path": "marco_ce_config.json",
+        "use_cuda": True,
+        "gpu_card_id": 1,
+        "batch_size": 32
+    }
+
 
     data_list = []
-    for line in open("../../data/dev.qtpl.top50"):
+    for line in open("../data/dev.qtpl.top50"):
         v = line.strip().split('\t')
         data_list.append('\t'.join(v[:3]))
 
     #EncoderExample(para_encoder_conf, data_list, 'para')
     #EncoderExample(query_encoder_conf, data_list, 'query')
-    RankerExample(rank_conf, data_list)
+    #RankerExample(rank_conf, data_list)
+
+    EncoderInferExample(query_encoder_infer_conf, data_list, 'query')
+    EncoderInferExample(para_encoder_infer_conf, data_list, 'para')
+    #RankerExampleInfer(rank_infer_conf, data_list)
---
+++ rocketqa/predict/cross_encoder_infer.py
@@ -0,0 +1,125 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Finetuning on classification tasks."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import os
+import json
+import multiprocessing
+import numpy as np
+
+# NOTE(paddle-dev): All of these flags should be
+# set before `import paddle`. Otherwise, it would
+# not take any effect.
+os.environ['FLAGS_eager_delete_tensor_gb'] = '0'  # enable gc
+
+import paddle.fluid as fluid
+
+from reader import reader_ce_predict
+from model.ernie import ErnieConfig
+from model.cross_encoder_model import create_model
+from utils.args import print_arguments, check_cuda, prepare_logger
+from utils.init import init_pretraining_params, init_checkpoint
+from utils.finetune_args import parser
+
+
+class CrossEncoderInfer(object):
+    def __init__(self, conf_path, use_cuda, gpu_card_id, batch_size):
+        args = self._parse_args(conf_path)
+        args.use_cuda = use_cuda
+        self.batch_size = batch_size
+        ernie_config = ErnieConfig(args.ernie_config_path)
+        ernie_config.print_config()
+
+        if use_cuda:
+            dev_list = fluid.cuda_places()
+            place = dev_list[gpu_card_id]
+            dev_count = len(dev_list)
+        else:
+            place = fluid.CPUPlace()
+            dev_count = int(os.environ.get('CPU_NUM', multiprocessing.cpu_count()))
+        self.exe = fluid.Executor(place)
+
+        self.reader = reader_ce_predict.CEPredictorReader(
+            vocab_path=args.vocab_path,
+            label_map_config=args.label_map_config,
+            max_seq_len=args.max_seq_len,
+            total_num=args.train_data_size,
+            do_lower_case=args.do_lower_case,
+            in_tokens=args.in_tokens,
+            random_seed=args.random_seed,
+            tokenizer=args.tokenizer,
+            for_cn=args.for_cn,
+            task_id=args.task_id)
+
+        self.infer_program, feed_target_names, self.probs = fluid.io.load_inference_model(
+            args.init_checkpoint, self.exe, model_filename='model', params_filename='params')
+        #for fea in feed_target_names:
+        #    print(fea)
+        self.src_ids = feed_target_names[0]
+        self.sent_ids = feed_target_names[1]
+        self.pos_ids =  feed_target_names[2]
+        self.input_mask = feed_target_names[3]
+
+
+    def _parse_args(self, conf_path):
+        args = parser.parse_args()
+        try:
+            with open(conf_path, 'r', encoding='utf8') as json_file:
+                config_dict = json.load(json_file)
+        except Exception:
+            raise IOError("Error in parsing model config file '%s'" %conf_path)
+
+        args.do_train = False
+        args.do_val = False
+        args.do_test = True
+        args.use_fast_executor = True
+        args.max_seq_len = config_dict['max_seq_len']
+        args.ernie_config_path = config_dict['model_conf_path']
+        args.vocab_path = config_dict['model_vocab_path']
+        args.init_checkpoint = config_dict['model_checkpoint_path']
+
+        return args
+
+
+    def get_rank_score(self, data):
+
+        predict_data_generator = self.reader.data_generator(
+                  data,
+                  batch_size=self.batch_size,
+                  epoch=1,
+                  dev_count=1,
+                  shuffle=False)
+
+        all_results = []
+        for sample in predict_data_generator():
+            src_ids_data = sample[0]
+            sent_ids_data = sample[1]
+            pos_ids_data = sample[2]
+            task_ids_data = sample[3]
+            input_mask_data = sample[4]
+            output = self.exe.run(
+                self.infer_program,
+                feed={self.src_ids: src_ids_data,
+                      self.sent_ids: sent_ids_data,
+                      self.pos_ids: pos_ids_data,
+                      self.input_mask: input_mask_data},
+                fetch_list=self.probs)
+            all_results.extend(output[0][:,1])
+        return all_results
